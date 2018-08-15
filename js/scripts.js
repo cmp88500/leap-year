35e3db73c9fddfbc4f8a3b8dee34087d0ca14ed5
Cam Peters and Josh Gearheart <josh@>
2018-08-15 10:09:20 -0700

    Set up JS/jQuery

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(function(){
+  console.log("i am working");
+
+  yearInput.each( function() {
+    var yearInput = $("#leap-year").val();
+
+    return false
+});
+});
+
+var leapyear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year".val());
+    var result = leapYear(year);
+
+  $(".year").text(year);
+
+  if (!result) {
+    $(".not").text("not");
+  } else {
+    $(".not").text("");
+  }
+
+  $("#result").show();
+});
+});
